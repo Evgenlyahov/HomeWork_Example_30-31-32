fec9cec64917a97d477787055bbdf9876b8ade39
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-14 18:49:26 +0300

    Example 30

---
+++ Exemple 30/Program.cs
@@ -0,0 +1,13 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+int digit = new Random().Next(1, 21);
+System.Console.WriteLine($"Случайное число {digit}");
+int kub = digit*digit*digit;
+if (kub%2 == 0)
+{
+    System.Console.WriteLine($"Куб числа {digit} оканчивается на чётную цифру и равен {kub}");
+}
+else
+{
+    System.Console.WriteLine($"Куб числа {digit} оканчивается на нечётную цифру");
+}
